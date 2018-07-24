e571c66f6295f5c809453667419b76aa4bf2607d
geirhei <geirhene@gmail.com>
2018-07-24 21:11:40 +0200

    disabled init() for most functionality in main.cpp

--- main.cpp
+++ main.cpp
@@ -784,26 +784,26 @@ static void advertising_start(bool erase_bonds)
  */
 int main(void)
 {
-	bool erase_bonds;
+	//bool erase_bonds;
 
 	// Initialize.
-	log_init();
-	timers_init();
-	buttons_leds_init(&erase_bonds);
+	//log_init();
+	//timers_init();
+	//buttons_leds_init(&erase_bonds);
 	power_management_init();
-	ble_stack_init();
-	gap_params_init();
-	gatt_init();
-	advertising_init();
+	//ble_stack_init();
+	//gap_params_init();
+	//gatt_init();
+	//advertising_init();
 	services_init();
-	conn_params_init();
-	peer_manager_init();
+	//conn_params_init();
+	//peer_manager_init();
 
 	// Start execution.
-	NRF_LOG_INFO("Template example started.");
-	application_timers_start();
+	//NRF_LOG_INFO("Template example started.");
+	//application_timers_start();
 
-	advertising_start(erase_bonds);
+	//advertising_start(erase_bonds);
 
 	// Robot testing begin //
 	Robot testRobot;
